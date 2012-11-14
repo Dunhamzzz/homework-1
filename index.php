306fe9f3a79286ecb80a60e3f58616c884147974
Matthew Dunham <me@dunhamzzz.com>
2012-11-14 23:12:37 +0000

    base setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php ?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Homework #1</title>
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+    </head>
+    <body>
+        <h1>Contact Form</h1>
+        
+    </body>
+</html>
